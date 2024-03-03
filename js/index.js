253ab65f7e900c006ed4b53ec1f85e2e144c5dae
changemeback <mpazbbb@gmail.com>
2024-03-03 21:36:47 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 console.log('Hello World');
+$(".burger-wrap").click(function () {
+    $(".header-main_nav").toggleClass("header-main_nav--open");
+    $(".burger").toggleClass('burger--close');
+});
